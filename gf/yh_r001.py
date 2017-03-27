66adc12508e296b27109113adce78c63c8731575
allenwwg <allenwwg@qq.com>
2017-03-27 18:27:28 +0800

    Update yh_r001.py
    call web API for r001

--- gf/yh_r001.py
+++ gf/yh_r001.py
@@ -1,21 +1,8 @@
 # encoding: UTF-8
+import urllib.request
 
-import easytrader
-from mail import *
+requrl = 'http://allenwwg.cn/bR01/taurus'
+msg = requests.get(requrl)
+print(msg)
 
-user = easytrader.use('yh',debug=False)
-user.prepare('c:\yh.json')
-aviliable_money = user.balance[0]['可用资金']
-
-msg = ''
-if aviliable_money > 1000:
-	sell_amount = int(aviliable_money / 1000)*10
-	result = user.sell('131810',1.0, sell_amount);
-	msg = str(result)
-	print(msg)
-else :
-	msg = 'No enough money for r-001'
-	print (msg)
-   
-user.exit()
 send_email(subject = msg,body = '')
